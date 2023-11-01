ea863bb5889764f38c4a9d453ef8113d6acdbbe8
eyedeekay <idk@mulder>
2023-11-01 17:46:32 -0400

    add go-apt-mirror version

--- go-apt-mirror.sh
+++ go-apt-mirror.sh
@@ -3,3 +3,5 @@ docker container run \
     -v $(pwd)/etc/go-apt-mirror.toml:/etc/apt/mirror.toml:ro \
     -v $(pwd)/debian_mirror:/var/spool/go-apt-mirror \
     jacksgt/aptutil /go-apt-mirror
+mirrordir=$(ls -d $(pwd)/debian_mirror/.ubuntu*/ubuntu | tail -n 1)
+cp -rv $mirrordir $(pwd)/debian_mirror/

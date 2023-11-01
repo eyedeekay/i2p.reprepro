07778889be1588e0fdb106e646c77ed4210815c7
eyedeekay <idk@mulder>
2023-11-01 16:41:48 -0400

    Mirror: update script

--- reprepro.sh
+++ reprepro.sh
@@ -4,11 +4,8 @@ do
 	reprepro -v --noskipold -b ./debian_reprepro --confdir ./debian/conf --logdir ./debian_logs update $i
 done
 
-reprepro -v -b ./debian_reprepro --confdir ./debian/conf --logdir ./debian_logs copysrc sid lunar i2p-keyring
-reprepro -v -b ./debian_reprepro --confdir ./debian/conf --logdir ./debian_logs copysrc bookworm focal i2p-keyring
-reprepro -v -b ./debian_reprepro --confdir ./debian/conf --logdir ./debian_logs copysrc bullseye focal i2p-keyring
-reprepro -v -b ./debian_reprepro --confdir ./debian/conf --logdir ./debian_logs copysrc buster focal i2p-keyring
 reprepro -v -b ./debian_reprepro --confdir ./debian/conf --logdir ./debian_logs copysrc sid lunar i2p
 reprepro -v -b ./debian_reprepro --confdir ./debian/conf --logdir ./debian_logs copysrc bookworm focal i2p
 reprepro -v -b ./debian_reprepro --confdir ./debian/conf --logdir ./debian_logs copysrc bullseye focal i2p
 reprepro -v -b ./debian_reprepro --confdir ./debian/conf --logdir ./debian_logs copysrc buster focal i2p
+chown -R 1000:1000 ./debian_reprepro

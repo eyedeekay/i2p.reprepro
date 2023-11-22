c2c565fc2e7104a9f585c33eeaf5ac5d132d6aef
eyedeekay <idk@mulder>
2023-11-21 23:29:26 -0500

    add curlpipe script

---
+++ curlpipe.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env sh
+
+# get our distro codename
+export DIST="$(lsb_release -cs)"
+echo "deb [signed-by=/usr/share/keyrings/i2p-archive-keyring.gpg] https://deb.i2pgit.org/debian-experimental/ $DIST main" \
+  | sudo tee /etc/apt/sources.list.d/deb.i2pgit.org.list
